0c2c5e2db49060c016bee56fc39456f9951cf6ff
DorianGrey <ShadowVampire@web.de>
2017-04-29 14:31:14 +0200

    feat(production mode): Improve conditional rendering of react-hot-loaders 'AppContainer' element.

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,18 @@ import "./styles/main.scss";
 import * as React from "react";
 import {render} from "react-dom";
 import {AppContainer} from "react-hot-loader";
+import identity from "lodash-es/identity";
 
 import {bootloader} from "./bootloader";
 import App from "./app/App";
+
+// We don't need the 'AppContainer' wrapper in production mode, thus, we make a difference here.
+let wrapWithRhlContainer: <P extends React.DOMAttributes<T>, T extends Element>(s: React.DOMElement<P, T>) => React.DOMElement<P, T>;
+if (process.env.NODE_ENV === "production") {
+  wrapWithRhlContainer = identity;
+} else {
+  wrapWithRhlContainer = <P extends React.DOMAttributes<T>, T extends Element>(elem: React.DOMElement<P, T>) => <AppContainer>{elem}</AppContainer> as React.DOMElement<P, T>;
+}
 
 function getContainer() {
   return document.getElementById("app");
@@ -14,19 +23,7 @@ function getContainer() {
 function renderApp(container: HTMLElement | null) {
   container = container || getContainer();
 
-  if (process.env.NODE_ENV === "production") { // We don't need the 'AppContainer' wrapper in production mode, thus, we make a difference here.
-    return render(
-      <App />,
-      container
-    );
-  } else {
-    return render(
-      <AppContainer>
-        <App />
-      </AppContainer>,
-      container
-    );
-  }
+  return render(wrapWithRhlContainer(<App /> as React.DOMElement<any, any>), container);
 }
 
 function main() {
