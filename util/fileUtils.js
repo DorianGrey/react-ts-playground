610abcef139673916b466a88897950b10f0fdb7d
Christian Linne <ShadowVampire@web.de>
2020-07-03 10:14:00 +0200

    Move to pnpm instead of yarn on master (#14)

--- util/fileUtils.js
+++ util/fileUtils.js
@@ -1,30 +1,21 @@
 "use strict";
 
-const fs = require("fs");
+const fs = require("fs-extra");
 const _ = require("lodash");
 const mkdirp = require("mkdirp");
-const glob = require("glob");
+const glob = require("globby");
 const path = require("path");
 
 module.exports = {
-  getFiles(src, options) {
-    return new Promise((resolve, reject) =>
-      glob(src, options || {}, (e, files) => (e ? reject(e) : resolve(files)))
-    ).then((files) => _.reject(files, (f) => _.includes(f, "*")));
+  async getFiles(src, options) {
+    const files = await glob(src, options || {});
+    return _.reject(files, (f) => _.includes(f, "*"));
   },
   readFile(src) {
-    return new Promise((resolve, reject) =>
-      fs.readFile(src, "utf-8", (e, content) =>
-        e ? reject(e) : resolve(content)
-      )
-    );
+    return fs.readFile(src, "utf-8");
   },
   async writeFile(dest, content, options) {
     await mkdirp(path.dirname(dest));
-    return await new Promise((resolve, reject) =>
-      fs.writeFile(dest, content, options || {}, (e) =>
-        e ? reject(e) : resolve()
-      )
-    );
+    return fs.writeFile(dest, content, options || {});
   },
 };
